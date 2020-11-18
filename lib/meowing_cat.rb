dca82ccaf749f4269fa680a8744f3410b7b298ce
Martinique <me@martidolce.com>
2020-11-18 02:37:23 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,20 @@
-## code your solution here. 
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+#
+class Cat
+	attr_accessor :name, :meow
+	def cat(name)
+		@name = name
+	end
+	def meow
+		puts "meow!"
+	end
+	end
+
+maru = Cat.new
+maru.name = "Maru"
+maru.meow
